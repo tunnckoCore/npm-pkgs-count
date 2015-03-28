fb0d24670108a26bf528bd6e8cac539c385b7140
tunnckoCore <mameto_100@mail.bg>
2015-03-28 15:35:35 +0200

    add tests

--- test.js
+++ test.js
@@ -8,8 +8,63 @@
 'use strict';
 
 var assert = require('assert');
-var npmPkgsCount = require('./index');
+var npmPkgs = require('./index');
 
 describe('npm-pkgs-count:', function() {
-  // body
+  it('should throw TypeError when `username` is not a string', function(done) {
+    assert.throws(function fixture() {
+      npmPkgs({one: 'two'});
+    }, /expect `username` to be string/);
+
+    assert.throws(function fixture() {
+      npmPkgs({one: 'two'});
+    }, TypeError);
+    done();
+  });
+
+
+  it('should throw Error when `username` is an empty string or array', function(done) {
+    assert.throws(function fixture() {
+      npmPkgs('');
+    }, /expect `username` to be non empty string/);
+
+    assert.throws(function fixture() {
+      npmPkgs('');
+    }, Error);
+
+    done();
+  });
+
+  it('should throw TypeError when `callback` is not a function', function(done) {
+    assert.throws(function fixture() {
+      npmPkgs('tunnckocore', [1, 2, 3]);
+    }, /expect `callback` to be/);
+
+    assert.throws(function fixture() {
+      npmPkgs('tunnckocore', [1, 2, 3]);
+    }, TypeError);
+    done();
+  });
+
+  it('should work properly when existing user given and callback', function(done) {
+    this.timeout(30000);
+
+    npmPkgs('tunnckocore', function _cb(err, res) {
+      assert.ifError(err);
+      assert.strictEqual(res > 90, true);
+      assert.strictEqual(!Array.isArray(res), true);
+      done();
+    });
+  });
+
+  it('should error when non existing user given', function(done) {
+    this.timeout(30000);
+
+    npmPkgs('fjk43hkjhhhhhhhhhhhhhhhkjgg3k4g234', function _cb(err, res) {
+      assert.strictEqual(err instanceof Error, true);
+      assert.strictEqual(err.code, 404);
+      assert.strictEqual(res, undefined);
+      done();
+    });
+  });
 });
